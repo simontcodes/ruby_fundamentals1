8050840248a752e71ef0ab9a98896cce1f3445bd
stangar <simon.tang.arias@gmail.com>
2016-05-31 13:49:43 -0400

    exercise2

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+#1 tip for a 55 dollar meal
+puts 55 * 0.20
+
+# you cant add string with numbers
+
+puts 1.2 + "holaaa"
+
+#string interpolation
+
+puts "this is an example of string interpolation #{45628 * 7839}"
+
+# comparadores
+
+(10<20 && 30<20) || !(10 == 11)
